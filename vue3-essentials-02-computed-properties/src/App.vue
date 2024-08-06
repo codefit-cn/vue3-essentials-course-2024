664dc49e4d872a4dbcecda4044d7046e5d31e35f
CodeFit <codefit@qq.com>
2024-08-06 18:14:20 +0800

    🐳 chore: 创建计算属性的案例项目

---
+++ vue3-essentials-02-computed-properties/src/App.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const count = ref(18)
+    function increase() {
+      count.value++
+    }
+
+    return {
+      count,
+      increase
+    }
+  }
+})
+</script>
+
+<template>
+  <main>
+    <h1>金币：{{ count }}</h1>
+    <button @click="increase">增加金币</button>
+    <button :disabled="count < 20">{{ count >= 20 ? '可以参加' : '不够金币，不可以参加' }}</button>
+    <p>"Hello, Vue.js 3.x 必修课｜2024 By CodeFit"</p>
+  </main>
+</template>
