50752b1524170cc253da0a93705dde51403bf76b
CodeFit <codefit@qq.com>
2024-08-07 20:22:44 +0800

    🐳 chore: 完成计算属性案例

--- vue3-essentials-02-computed-properties/src/App.vue
+++ vue3-essentials-02-computed-properties/src/App.vue
@@ -1,17 +1,25 @@
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { computed, defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'App',
   setup() {
     const count = ref(18)
+
+    const buttonStatus = computed<{ text: string; disabled: boolean }>(() => {
+      return {
+        text: count.value >= 20 ? '可以参加' : '不够金币，不可以参加',
+        disabled: count.value < 20
+      }
+    })
     function increase() {
       count.value++
     }
 
     return {
       count,
-      increase
+      increase,
+      buttonStatus
     }
   }
 })
@@ -21,7 +29,7 @@ export default defineComponent({
   <main>
     <h1>金币：{{ count }}</h1>
     <button @click="increase">增加金币</button>
-    <button :disabled="count < 20">{{ count >= 20 ? '可以参加' : '不够金币，不可以参加' }}</button>
+    <button :disabled="buttonStatus.disabled">{{ buttonStatus.text }}</button>
     <p>"Hello, Vue.js 3.x 必修课｜2024 By CodeFit"</p>
   </main>
 </template>
